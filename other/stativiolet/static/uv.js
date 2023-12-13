df8dd2ff56a4d2364dca9da54e801df27a666e78
EagleNetwork <153021443+eaglenetwork@users.noreply.github.com>
2023-12-13 15:03:26 -0800

    Update uv.js

--- other/stativiolet/static/uv.js
+++ other/stativiolet/static/uv.js
@@ -1,8 +1,7 @@
-importScripts('/static/uv/uv.bundle.js');
-importScripts('/static/uv/uv.config.js');
-importScripts('/static/uv/uv.sw.js');
+importScripts('/other/stativiolet/static/uv/uv.bundle.js');
+importScripts('/other/stativiolet/static/uv/uv.config.js');
+importScripts('/other/stativiolet/static/uv/uv.sw.js');
 importScripts('https://arc.io/arc-sw-core.js');
-
 const sw = new UVServiceWorker();
 
 self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
